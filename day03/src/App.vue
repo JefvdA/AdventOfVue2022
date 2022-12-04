caec1871629b1591d36ad247cd0afd7f4388a68d
Jef van der Avoirt <jef.v.d.a@live.be>
2022-12-04 22:05:36 +0000

    add day03 solution

---
+++ day03/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import CountdownHeader from '@/components/CountdownHeader.vue'
+import CountdownSegment from './components/CountdownSegment.vue'
+import { useNow } from '@vueuse/core'
+import { computed, Transition } from 'vue'
+
+const now = useNow()
+const christmas = new Date('12/25/2022 00:00:00')
+
+const diff = computed(() => christmas.getTime() - now.value.getTime())
+
+const days = computed(() => Math.floor(diff.value / (1000 * 60 * 60 * 24)))
+const hours = computed(() => Math.floor((diff.value / (1000 * 60 * 60)) % 24))
+const minutes = computed(() => Math.floor((diff.value / (1000 * 60)) % 60))
+const seconds = computed(() => Math.floor((diff.value / 1000) % 60))
+</script>
+<template>
+  <div class="w-full h-full flex justify-center items-center p-10">
+    <div>
+      <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
+        <CountdownHeader />
+        <main class="flex justify-center">
+          <CountdownSegment label="days" :number="days" />
+          <CountdownSegment label="hours" :number="hours" />
+          <CountdownSegment label="minutes" :number="minutes" />
+          <CountdownSegment label="seconds" :number="seconds" />
+        </main>
+      </div>
+      <h4 class="mt-10 text-gray-400 text-center text-sm">
+        This challenge brought to you by <a href="https://vueschool.io/" class="underline">Vue School</a>
+      </h4>
+    </div>
+  </div>
+</template>
+
+<style>
+div {
+  display: block;
+}
+
+body {
+  @apply bg-gray-100;
+}
+</style>
---
+++ day03/src/components/CountdownSegment.vue
@@ -0,0 +1,44 @@
+<script setup>
+defineProps({
+  label: String,
+  number: Number,
+})
+</script>
+<template>
+  <div class="segment text-center">
+    <div class="pt-10 overflow-hidden relative">
+      <Transition>
+        <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      </Transition>
+    </div>
+
+    <span class="label block pt-2">{{ label }}</span>
+  </div>
+</template>
+<style>
+.segment {
+  width: 80px;
+}
+.numbers {
+  transform: translateX(-50%);
+  font-size: 32px;
+}
+.label {
+  font-size: 16px;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(80%) translateX(-50%);
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-80%) translateX(-50%);
+}
+</style>
