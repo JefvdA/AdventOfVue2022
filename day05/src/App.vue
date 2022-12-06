cea72805fe163d5fc82ca118e2ca1954c66c2c9b
Jef van der Avoirt <jef.v.d.a@live.be>
2022-12-06 13:58:16 +0000

    add day05

---
+++ day05/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import GiftLabel from './components/GiftLabel.vue'
+
+const from = ref('')
+const to = ref('')
+</script>
+
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <div>
+      <GiftLabel :from="from" :to="to" />
+      <div class="mt-6">
+        <label class="block">
+          To:
+          <input
+            v-model="to"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
+          />
+        </label>
+        <label class="mt-2 block">
+          From:
+          <input
+            v-model="from"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
+          />
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
